16f894ca9993b7a76034f28568d1937d18ec099b
Johnny Hall <johnny@recipher.co.uk>
2016-01-21 12:13:08 +0000

    Remove custom test task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,5 @@
-// var gulp = require('gulp');
+var gulp = require('gulp');
 
-var gulp = require('gulp')
-  , babel = require('gulp-babel')
-  , mocha = require('gulp-mocha');
-
-require('@ftbl/gulp')(gulp);
-require('babel-core/register');
-
-gulp.task('test', function() {
-  return gulp.src('test/**/*.js', { read: false, presets: [ 'es2015' ] })
-  .pipe(babel())
-  .pipe(mocha());
-});
+require('@ftbl/gulp')(gulp, { test: { coverage: 65 }});
 
 gulp.task('default', [ 'test' ]);
